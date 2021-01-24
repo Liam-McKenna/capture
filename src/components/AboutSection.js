34e23a1c2c93340bd1ff2d0a7bc0309a1ef654d0
Liam-McKenna <liam.zendesign@gmail.com>
2021-01-24 21:10:42 +0000

    .5 intro to styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,23 +1,25 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//Styled
+import styled from "styled-components";
 
 //change name to <Liam McKenna  />
 const AboutSection = () => {
   return (
-    <div className="about-AboutSection">
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>Liam McKenna</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               A Design <span>&</span> Development
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>Portfolio</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           MERN full stack developer based in Dublin. Degree in Information
@@ -25,10 +27,46 @@ const AboutSection = () => {
           Applications, Databases and Cloud Systems
         </p>
         <button>Contact Me</button>
-      </div>
-      <img src={home1} alt="ProfilePhoto" /> {/*change to picture of me&Logo*/}
-    </div>
+      </Description>
+
+      <Image>
+        <img src={home1} alt="ProfilePhoto" />{" "}
+        {/*change to picture of me&Logo*/}
+      </Image>
+    </About>
   );
 };
+
+//Styled Components
+const About = styled.div`
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+}
+
+body{
+    background: #1b1b1b;
+}
+
+button{
+font-weight:bold;
+font-size: 1.1.rem;
+cursor: cursor;
+padding: 1rem 2rem;
+border: 3px solid #d9235a;
+background:transparent;
+color:white;
+transition: all 3.5s ease;
+
+&:hover{
+    background-color: #d9235a;
+    color:white;
+    }
+  
+}
+
+h2{
+    font-weight: lighter;
+    font-size: 4rem;
+}
+h3{
+    color:white;
+}
+h4{
+    //font-weight: bold;
+    color: #d9235a;
+}
+span{
+    font-weight:bold;
+    color: #d9235a;
+
+}
+a{
+    font-size:1.1rem;
+}
+p{
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
+}
+`;
+
+export default GlobalStyle;

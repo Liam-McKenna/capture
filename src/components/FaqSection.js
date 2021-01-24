fbdc74ce7f3cd809886f7f48c5b67bd4448a296f
Liam-McKenna <liam.zendesign@gmail.com>
2021-01-24 18:50:07 +0000

    4. faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="qustion">
+        <h4>How Do I start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            voluptatibus aspernatur illum sapiente labore necessitatibus, aut
+            eum id obcaecati, maiores magnam a sequi provident vero sint iusto
+            autem nostrum quibusdam?
+          </p>
+        </div>
+      </div>
+      <div className="qustion">
+        <h4>daily schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            voluptatibus aspernatur illum sapiente labore necessitatibus, aut
+            eum id obcaecati, maiores magnam a sequi provident vero sint iusto
+            autem nostrum quibusdam?
+          </p>
+        </div>
+      </div>
+      <div className="qustion">
+        <h4>Payment Methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            voluptatibus aspernatur illum sapiente labore necessitatibus, aut
+            eum id obcaecati, maiores magnam a sequi provident vero sint iusto
+            autem nostrum quibusdam?
+          </p>
+        </div>
+      </div>
+      <div className="qustion">
+        <h4>What Products Do you offer</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+            voluptatibus aspernatur illum sapiente labore necessitatibus, aut
+            eum id obcaecati, maiores magnam a sequi provident vero sint iusto
+            autem nostrum quibusdam?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;

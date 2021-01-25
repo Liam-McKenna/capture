40ebf402726d5ec389e280e21999287b7c5142cc
Liam-McKenna <liam.zendesign@gmail.com>
2021-01-25 19:48:31 +0000

    7. autosave

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import Nav from "./components/Nav";
 
 //import pages
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+
+//router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
